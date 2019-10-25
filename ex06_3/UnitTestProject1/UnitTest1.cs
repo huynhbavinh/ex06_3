3b982c09d4a64730c36ac5d740ef5438f1ba0039
huynhbavinh <huynhbavinh117@gmail.com>
2019-10-25 18:53:55 +0700

    another way for ex06_3

---
+++ ex06_3/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ex06_3;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestCreated()
+        {
+            Queue q1 = new Queue();
+            q1.En(1);
+            q1.En(2);
+            q1.En(3);
+            q1.En(4);
+            Assert.AreEqual(4, q1.CheckCount());
+        }
+        [TestMethod]
+        public void TestDe()
+        {
+            Queue q2 = new Queue();
+            q2.En(1);
+            q2.En(2);
+            q2.En(3);
+            q2.En(4);
+            Assert.AreEqual(1, q2.De());
+            Assert.AreEqual(2, q2.De());
+            Assert.AreEqual(3, q2.De());
+        }
+        [TestMethod]
+        public void TestPeek()
+        {
+            Queue q3 = new Queue();
+            q3.En(1);
+            q3.En(2);
+            q3.En(3);
+            q3.En(4);
+            Assert.AreEqual(1, q3.peek());
+        }
+    }
+}
---
+++ ex06_3/ex06_3/Queue.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex06_3
+{
+    public class Queue
+    {
+        private int[] space;
+        private int bot;
+        private int statue=0;
+        public Queue(int size)
+        {
+            space = new int [size];
+        }         
+        public Queue()
+        {
+            space = new int[100];
+            bot = -1;
+        }
+        public void En(int number)
+        {
+            space[bot + 1] = number;
+            statue++;
+            bot++;
+        }
+        public int De()
+        {
+            int temp = space[statue - bot - 1];
+            bot--;
+            return temp;
+        }
+        public int peek()
+        {
+            return space[statue - bot - 1];
+        }
+        public int CheckCount()
+        {
+            return bot+1;
+        }
+        public void clear()
+        {
+            bot = -1;
+            statue = 0;
+        }
+    }
+}
